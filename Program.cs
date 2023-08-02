f2c1fcfc50829740cb7070388078bb745399921f
crazle <crazle@mail.ru>
2023-08-02 13:07:36 +0300

    Сумма от M до N

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int FindSum(int m, int n)
+{
+    if (n == m) return m;
+    else return (m + FindSum(m+1, n));
+}
+
+int GetInput(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetInput("Введите число M:");
+int n = GetInput("Введите число N:");
+int result = (FindSum(m, n));
+Console.WriteLine($"{result}");
